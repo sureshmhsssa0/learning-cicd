1c781b41d66623ecc987ec16a335e38a88182975
suresh kumar <sureshmhsssa0@gmail.com>
2022-09-18 13:10:26 +0530

    first service

---
+++ src/main/java/com/mhgroup/learningcicd/controller/FirstController.java
@@ -0,0 +1,7 @@
+package com.mhgroup.learningcicd.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FirstController {
+}
---
+++ src/main/java/com/mhgroup/learningcicd/entity/First.java
@@ -0,0 +1,7 @@
+package com.mhgroup.learningcicd.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class First {
+}
