dac97cead1cf5735e36cf9f76e47d983ae51dc20
suresh kumar <sureshmhsssa0@gmail.com>
2022-09-18 10:38:56 +0530

    initial commit

---
+++ src/main/java/com/mhgroup/learningcicd/LearningCicdApplication.java
@@ -0,0 +1,13 @@
+package com.mhgroup.learningcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhgroup/learningcicd/LearningCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhgroup.learningcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
